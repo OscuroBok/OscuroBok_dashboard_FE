fe6399d54d5ff7aee7e9e7e2e83761314e865313
Abir Dutta <abirdutta390@gmail.com>
2024-08-13 22:09:21 +0530

    desktop-with-exisiting-image

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -7,84 +7,89 @@ import {
 } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
+import { useForm } from "react-hook-form"
+
 
 export function SignUp() {
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors,isSubmitting },
+  } = useForm()
+
+
+  const onSubmit = (data) => console.log(data)
+
   return (
     <section className="m-8 flex">
-            <div className="w-2/5 h-full hidden lg:block">
+      <div className="w-2/5 h-full hidden lg:block">
         <img
           src="/img/pattern.png"
           className="h-full w-full object-cover rounded-3xl"
         />
       </div>
-      <div className="w-full lg:w-3/5 flex flex-col items-center justify-center">
-        <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Join Us Today</Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to register.</Typography>
+      <div className="w-full lg:w-[740px] flex flex-col items-center justify-center">
+        <div className="">
+          <Typography variant="h3" className="h-[42px] text-[#525252]  mb-4">Create Account</Typography>
+          <div className="flex gap-3 ">
+            <button className="h-[40px]  flex gap-1 rounded-lg text-[#A1A1A1] text-[16px] border border-[#E8E8E8] px-4 py-2">
+            <img src="../../../public/img/logo-google.png" /> Continue with Google</button>
+            <button className="h-[40px]  flex gap-1 rounded-lg text-[#A1A1A1] text-[16px] border border-[#E8E8E8] px-4 py-2"><img src="../../../public/img/logo-facebook.png" />Continue with Facebook</button>
+          </div>
         </div>
-        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
-          <div className="mb-1 flex flex-col gap-6">
-            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Your email
-            </Typography>
-            <Input
-              size="lg"
-              placeholder="name@mail.com"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-          </div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center justify-start font-medium"
-              >
-                I agree the&nbsp;
-                <a
-                  href="#"
-                  className="font-normal text-black transition-colors hover:text-gray-900 underline"
-                >
-                  Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          />
-          <Button className="mt-6" fullWidth>
-            Register Now
-          </Button>
+        <form className="mt-10 flex flex-col gap-7" onSubmit={handleSubmit(onSubmit)}>
+          {/* Full Name Input */}
+          <input className="h-[29px] text-[14px] font-[500] w-[464px] border-b px-2 py-1 focus:outline-none " placeholder="Full Name" {...register("fullName",{required: true})} />
+          {errors.fullName && <span className="text-red-400">Name cannot be empty!</span>}
 
-          <div className="space-y-4 mt-8">
-            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
-              <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0_1156_824)">
-                  <path d="M16.3442 8.18429C16.3442 7.64047 16.3001 7.09371 16.206 6.55872H8.66016V9.63937H12.9813C12.802 10.6329 12.2258 11.5119 11.3822 12.0704V14.0693H13.9602C15.4741 12.6759 16.3442 10.6182 16.3442 8.18429Z" fill="#4285F4" />
-                  <path d="M8.65974 16.0006C10.8174 16.0006 12.637 15.2922 13.9627 14.0693L11.3847 12.0704C10.6675 12.5584 9.7415 12.8347 8.66268 12.8347C6.5756 12.8347 4.80598 11.4266 4.17104 9.53357H1.51074V11.5942C2.86882 14.2956 5.63494 16.0006 8.65974 16.0006Z" fill="#34A853" />
-                  <path d="M4.16852 9.53356C3.83341 8.53999 3.83341 7.46411 4.16852 6.47054V4.40991H1.51116C0.376489 6.67043 0.376489 9.33367 1.51116 11.5942L4.16852 9.53356Z" fill="#FBBC04" />
-                  <path d="M8.65974 3.16644C9.80029 3.1488 10.9026 3.57798 11.7286 4.36578L14.0127 2.08174C12.5664 0.72367 10.6469 -0.0229773 8.65974 0.000539111C5.63494 0.000539111 2.86882 1.70548 1.51074 4.40987L4.1681 6.4705C4.8001 4.57449 6.57266 3.16644 8.65974 3.16644Z" fill="#EA4335" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_1156_824">
-                    <rect width="16" height="16" fill="white" transform="translate(0.5)" />
-                  </clipPath>
-                </defs>
-              </svg>
-              <span>Sign in With Google</span>
-            </Button>
-            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
-              <img src="/img/twitter-logo.svg" height={24} width={24} alt="" />
-              <span>Sign in With Twitter</span>
-            </Button>
+          {/* Login Type */}
+          <select
+        className="h-[29px] w-[464px] text-[14px] font-[500]  border-b-2 px-2 py-1 focus:outline-none bg-white text-[#A1A1A1]"
+        {...register("dropdown", { required: "Please select an option" })}
+      >
+        <option value="" disabled selected hidden>Log in as:</option>
+        <option value="user" className="text-[#464F60] text-[14px] font-[500]">User</option>
+        <option value="superAdin" className="text-[#D1293D] text-[14px] font-[500]">Super Admin</option>
+        <option value="Vendor" className="text-[#FF964F] text-[14px] font-[500]">Vendor</option>
+      </select>
+      {errors.dropdown && <span className="text-red-400">{errors.dropdown.message}</span>}
+
+          {/* Email Input */}
+          <input className="h-[29px] text-[14px] font-[500] w-[464px] border-b px-2 py-1 focus:outline-none" placeholder="Email Address" {...register("email",{required: true})} />
+          {errors.email && <span className="text-red-400">Please give correct email!</span>}
+
+          {/* Password Input */}
+          <input type="password" className="h-[29px] text-[14px] font-[500] w-[464px] border-b px-2 py-1 focus:outline-none" placeholder="Password" {...register("Password", { 
+          required: "Password is required", 
+          minLength: { value: 8, message: "Password must be at least 8 characters long" }, 
+          maxLength: { value: 20, message: "Password cannot exceed 20 characters" },
+          pattern: {
+            value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,20}$/,
+            message: "Password must include at least one uppercase letter, one lowercase letter, one digit."
+    }
+  })}  />
+          {errors.Password && <span className="text-red-400 max-w-md">{errors.Password.message}</span>}
+
+          {/* Phone Number Input */}
+          <input type="text" className="h-[29px] text-[14px] font-[500] w-[464px] border-b px-2 py-1 focus:outline-none" placeholder="Phone Number" {...register("phone", { required: true })} />
+          {errors.phone && <span className="text-red-400">Please provide with a phone number.</span>}
+
+          {/* Submit  */}
+          <div className="flex flex-col">
+            <button
+        type="submit"
+        className={`w-full py-2 text-white font-semibold text-lg rounded-md bg-[#8BB2B2] cursor-pointer hover:bg-[#76a4a4] ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''}`}
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? 'Submitting...' : 'Create Account'}
+      </button>
+            <span className="text-xm text-[#A1A1A1]">Already have an account? <Link to={'/auth/sign-in'}><span className="text-[#8BB2B2]">Login</span></Link> </span>
           </div>
-          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-            Already have an account?
-            <Link to="/auth/sign-in" className="text-gray-900 ml-1">Sign in</Link>
-          </Typography>
+         
         </form>
+          
 
       </div>
     </section>
