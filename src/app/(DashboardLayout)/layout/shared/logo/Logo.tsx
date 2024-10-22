01dd9b3ece7b0b855e8d564bf8314831a6632cc0
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-10-23 00:22:28 +0530

    fixed navbar logo

--- src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
@@ -3,35 +3,48 @@ import Link from "next/link";
 import { styled } from "@mui/material/styles";
 import { AppState } from "@/store/store";
 import Image from "next/image";
+import { Typography } from "@mui/material";
 
 const Logo = () => {
   const customizer = useSelector((state: AppState) => state.customizer);
   const LinkStyled = styled(Link)(() => ({
     height: customizer.TopbarHeight,
-    width: customizer.isCollapse ? "40px" : "180px",
+    width: customizer.isCollapse ? "80px" : "250px",
     overflow: "hidden",
-    display: "block",
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none", 
   }));
 
   if (customizer.activeDir === "ltr") {
     return (
       <LinkStyled href="/">
         {customizer.activeMode === "dark" ? (
-          <Image
-            src="/images/logos/light-logo.svg"
-            alt="logo"
-            height={customizer.TopbarHeight}
-            width={174}
-            priority
-          />
+          <>
+
+            <Image
+              src={"/images/logo-oscurobook.png"}
+              alt="logo"
+              
+              height={customizer.TopbarHeight}
+              width={80}
+              priority
+            />
+            <Typography variant="h2" sx={{color: customizer.activeMode === "dark" ? "white" : "black" }}  > oscurobook</Typography>
+          </>
         ) : (
-          <Image
-            src={"/images/logos/dark-logo.svg"}
-            alt="logo"
-            height={customizer.TopbarHeight}
-            width={174}
-            priority
-          />
+          <>
+
+            <Image
+              src={"/images/logo-oscurobook.png"}
+              alt="logo"
+              // sx={{ marginLeft: "8px", color: customizer.activeMode === "dark" ? "white" : "black" }}
+              height={customizer.TopbarHeight}
+              width={80}
+              priority
+            />
+            <Typography variant="h2" sx={{ color: customizer.activeMode === "dark" ? "white" : "black" }} > oscurobook</Typography>
+          </>
         )}
       </LinkStyled>
     );
